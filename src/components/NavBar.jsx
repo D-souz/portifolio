3365090f2825e46655e5e0f80e93d498d975cff3
D-souz <dhesouzanorbert36@gmail.com>
2024-05-16 18:10:26 +0300

    Creating the 3d landing page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <header className='header'>
+      <NavLink to="/" 
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">KJ</p>
+      </NavLink>
+      <nav className="flex font-medium text-lg gap-7">
+        <NavLink to="/about" 
+          className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black' }
+        >
+          <span>About</span>
+        </NavLink>
+        <NavLink to="/projects" 
+          className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black' }
+        >
+          <span>Projects</span>
+        </NavLink>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar;
