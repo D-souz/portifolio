6155ec81977f0fbbddc0fc3300c0e4a2805ac64a
D-souz <dhesouzanorbert36@gmail.com>
2024-06-03 14:52:18 +0300

    Added the rotating Island

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import  Contact from './pages/Contact';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
       <Router>
-        <NavBar />
+        {/* <NavBar /> */}
         <Routes>
           <Route path='/' element={'Home'}/>
           <Route path='/about' element={'About'}/>
